587643895a1788a9369dc2201c6645fc0e9608e0
Cleyson Oliveira <cleyson.gc@gmail.com>
2023-01-23 23:24:57 -0300

    adding some features

--- js/main.js
+++ js/main.js
@@ -1,8 +1,25 @@
 const form = document.querySelector("form")
 const nlwSetup = new NLWSetup(form)
+const button = document.querySelector("header button")
 
-const data = {
-  run: ["01-01", "01-02", "01-06"]
+button.addEventListener("click", add)
+form.addEventListener("change", save)
+
+function add() {
+  const today = new Date().toLocaleDateString("pt-br").slice(0, -5)
+  const dayExists = nlwSetup.dayExists(today)
+
+  if (dayExists) {
+    alert("Dia já incluso")
+    return
+  }
+
+  alert("Adicionado com sucesso")
+  nlwSetup.addDay(today)
+}
+
+function save() {
+  
 }
 
 nlwSetup.setData(data)
